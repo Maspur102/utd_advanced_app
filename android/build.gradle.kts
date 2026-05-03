6807fe4a47c704878173e1c68a7fd07f0e8f9917
Maspur102 <maspur15022005@gmail.com>
2026-05-03 17:52:00 +0700

    perbaikan kode di build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,10 +12,27 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// SKRIP PENYELAMAT NAMESPACE UNTUK ISAR_FLUTTER_LIBS (KOTLIN DSL)
+subprojects {
+    afterEvaluate {
+        project.extensions.findByName("android")?.let { androidExt ->
+            try {
+                val namespace = androidExt.javaClass.getMethod("getNamespace").invoke(androidExt)
+                if (namespace == null) {
+                    androidExt.javaClass.getMethod("setNamespace", String::class.java).invoke(androidExt, project.group.toString())
+                }
+            } catch (e: Exception) {
+                // Abaikan jika properti tidak ditemukan
+            }
+        }
+    }
 }
